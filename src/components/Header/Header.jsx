70ddd74911c36e377b12a307e83cf00bce9dc41d
Julian McNeill <julesdowork@gmail.com>
2025-03-29 21:22:39 -0400

    Add links to routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
+
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
 import menuIcon from "../../assets/menu-icon.png";
@@ -20,7 +22,9 @@ function Header({
   return (
     <header className="header">
       <div className="header__site-container">
-        <img src={logo} alt="WTWR logo" className="header__logo" />
+        <Link to="/se_project_react">
+          <img src={logo} alt="WTWR logo" className="header__logo" />
+        </Link>
         <button
           type="button"
           className="header__menu-btn header__menu-btn_visible"
@@ -48,10 +52,16 @@ function Header({
         >
           + Add clothes
         </button>
-        <div className="header__user-info">
-          <p className="header__username">Terrence Tegegne</p>
-          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
-        </div>
+        <Link to="/se_project_react/profile" className="header__link">
+          <div className="header__user-info">
+            <p className="header__username">Terrence Tegegne</p>
+            <img
+              src={avatar}
+              alt="Terrence Tegegne"
+              className="header__avatar"
+            />
+          </div>
+        </Link>
       </div>
     </header>
   );
