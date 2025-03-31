118b9a2a469872fa9bd246999b7ece1a18882702
Julian McNeill <julesdowork@gmail.com>
2025-03-31 19:29:40 -0400

    Build project 11

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./components/App/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* TODO: figure out how to use relative paths with HashRouter */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
